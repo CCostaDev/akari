3e92c5918990e32e4e301d4d8dcaad0619b20f45
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2025-07-24 21:36:19 +0100

    modified gif command to be able to send a gif to a selected channel

--- cogs/gif.py
+++ cogs/gif.py
@@ -7,13 +7,41 @@ class Gif(commands.Cog):
         self.bot = bot
 
     @commands.command(name="gif")
-    async def gif(self, ctx, *, search_term: str = "funny"):
-        """Sends a GIF based on the search term."""
-        gif_url = await fetch_gif(search_term)
-        if gif_url:
-            await ctx.send(gif_url)
-        else:
-            await ctx.send("Couldn't find a GIF for that 😔")
+    async def gif(self, ctx, *, args: str = "funny"):
+        """Sends a GIF to the mentioned channel (or current) based on the search term."""
+        words = args.split()
 
+        target_channel = ctx.channel
+        search_term = args
+
+        if words:
+            first = words[0]
+
+            #Check if the first word is a channel mention
+            if first.startswith("<#") and first.endswith(">"):
+                try:
+                    channel_id = int(first[2:-1])
+                    mentioned_channel = ctx.guild.get_channel(channel_id)
+                    if mentioned_channel:
+                        target_channel = mentioned_channel
+                        search_term = " ".join(words[1:]) or "funny"
+                except ValueError:
+                    pass  # Invalid mention format
+
+        gif_url = await fetch_gif(search_term)
+        if not gif_url:
+            await ctx.send("Couldn't find a GIF for that 😔")
+            return
+        
+        try:
+            await target_channel.send(gif_url)
+            if target_channel != ctx.channel:
+                await ctx.send(f"Sent your gif to {target_channel.mention}")
+        except discord.Forbidden:
+            await ctx.send("I don't have permission to send messages in that channel.")
+        except discord.HTTPException:
+            await ctx.send("Something went wrong when trying to send the GIF.")
+        
+        
 async def setup(bot):
     await bot.add_cog(Gif(bot))
